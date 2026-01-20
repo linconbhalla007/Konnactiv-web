496f1b86d57088e8d2ca7f562f6c9d0b6a593088
SANDEEP KUMAR BIHARI <sandeepbihari2005@gmail.com>
2026-01-20 18:39:48 +0530

    Added Popup in About page.

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -18,6 +18,10 @@ import services from "../assets/images/services.svg";
 import servicesMobile from "../assets/images/services-mobile.svg";
 import useWindowWidth from "../hooks/useWindowWidth";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import checkCircle from "../assets/images/check-circle.svg";
+import file from "../assets/images/file.svg";
+import cross from "../assets/images/cross.svg"; 
 
 export default function About() {
   const width = useWindowWidth();
@@ -25,6 +29,282 @@ export default function About() {
   const navigate = useNavigate();
   const handleContactClick = () => {
     navigate("/", { state: { scrollTo: "contact" } });
+  };
+    const [isContactOpen, setIsContactOpen] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  const useContactForm = () => {
+    const [formData, setFormData] = useState({
+      gender: "",
+      name: "",
+      phone: "",
+      email: "",
+      company: "",
+      description: "",
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (e: any) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const validate = () => {
+      const newErrors = {};
+
+      if (!formData.email.trim()) {
+        newErrors.email = "Pflichtfeld";
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+        newErrors.email = "Pflichtfeld";
+      }
+
+      return newErrors;
+    };
+
+    const handleSubmit = (e, onSuccess) => {
+      e.preventDefault();
+      const validationErrors = validate();
+      setErrors(validationErrors);
+
+      if (Object.keys(validationErrors).length === 0) {
+        onSuccess();
+      }
+    };
+
+    return {
+      formData,
+      errors,
+      handleChange,
+      handleSubmit,
+    };
+  };
+
+  const SuccessView = () => {
+    return (
+      <div className="d-flex justify-content-center align-items-center flex-column mt-3">
+        <img src={checkCircle} alt="checkCircle" />
+        <span className="success-text mt-3">
+          Vielen Dank für Ihre Anfrage. Wir melden uns innerhalb von 24 Stunden
+          bei Ihnen zurück.
+        </span>
+      </div>
+    );
+  };
+
+  const ContactFormWeb = ({ onSuccess }) => {
+    const { formData, errors, handleChange, handleSubmit } = useContactForm();
+    const [fileName, setFileName] = useState("");
+
+    return (
+      <form
+        className="contact-form mt-34"
+        onSubmit={(e) => handleSubmit(e, onSuccess)}
+      >
+        <div className="gender-container">
+          {["Herr", "Frau", "Divers"].map((item) => (
+            <label key={item} className="gender-option">
+              <input
+                type="radio"
+                name="gender"
+                value={item}
+                checked={formData.gender === item}
+                onChange={handleChange}
+              />
+              <span className="custom-radio"></span>
+              {item}
+            </label>
+          ))}
+        </div>
+        <div className="form-group w-100">
+          <input
+            type="text"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleChange}
+            name="name"
+          />
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email*"
+              value={formData.email}
+              onChange={handleChange}
+              name="email"
+              className={errors.email ? "input-error" : ""}
+            />
+            {errors.email && <span className="error-text">{errors.email}</span>}
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Telefon"
+              value={formData.phone}
+              onChange={handleChange}
+              name="phone"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Firma"
+            value={formData.company}
+            onChange={handleChange}
+            name="company"
+          />
+        </div>
+
+        <div className="form-group w-100">
+          <textarea
+            placeholder="Ihre Nachricht"
+            value={formData.description}
+            onChange={handleChange}
+            name="description"
+          ></textarea>
+        </div>
+
+        <div className="upload">
+          <label className="upload-btn">
+            <img src={file} alt="file" />
+            <span>Anhang</span>
+            <input
+              type="file"
+              hidden
+              onChange={(e) => setFileName(e.target.files[0]?.name || "")}
+            />
+          </label>
+          <span className="file-name-text">{fileName || "(Optional)"}</span>
+        </div>
+        <span className="file-max-size">
+          Die maximale Dateigröße beträgt 10 MB.
+        </span>
+        <div className="form-actions">
+          <button className="submit-btn-header w-50">Einreichen</button>
+          <button
+            type="button"
+            className="btn-cancel w-50"
+            onClick={() => setIsContactOpen(false)}
+          >
+            Abbrechen
+          </button>
+        </div>
+
+        <span className="file-max-size">
+          Mit Absenden akzeptieren Sie unsere AGB.
+        </span>
+      </form>
+    );
+  };
+
+  const ContactFormMobile = ({ onSuccess }) => {
+    const { formData, errors, handleChange, handleSubmit } = useContactForm();
+
+    const [fileName, setFileName] = useState("");
+
+    return (
+      <form
+        className="contact-form mt-52"
+        onSubmit={(e) => handleSubmit(e, onSuccess)}
+      >
+        <div className="gender-container">
+          {["Herr", "Frau", "Divers"].map((item) => (
+            <label key={item} className="gender-option">
+              <input
+                type="radio"
+                name="gender"
+                value={item}
+                checked={formData.gender === item}
+                onChange={handleChange}
+              />
+              <span className="custom-radio"></span>
+              {item}
+            </label>
+          ))}
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleChange}
+            name="name"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email*"
+            value={formData.email}
+            onChange={handleChange}
+            name="email"
+            className={errors.email ? "input-error" : ""}
+          />
+          {errors.email && <span className="error-text">{errors.email}</span>}
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Telefon"
+            value={formData.phone}
+            onChange={handleChange}
+            name="phone"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Firma"
+            value={formData.company}
+            onChange={handleChange}
+            name="company"
+          />
+        </div>
+
+        <div className="form-group w-100">
+          <textarea
+            placeholder="Ihre Nachricht"
+            value={formData.description}
+            onChange={handleChange}
+            name="description"
+          ></textarea>
+        </div>
+
+        <div className="upload">
+          <label className="upload-btn">
+            <img src={file} alt="file" />
+            <span>Anhang</span>
+            <input
+              type="file"
+              hidden
+              onChange={(e) => setFileName(e.target.files[0]?.name || "")}
+            />
+          </label>
+          <span className="file-name-text">{fileName || "(Optional)"}</span>
+        </div>
+        <span className="file-max-size">
+          Die maximale Dateigröße beträgt 10 MB.
+        </span>
+        <div className="form-actions">
+          <button className="submit-btn-header w-100">Einreichen</button>
+          <button
+            type="button"
+            className="btn-cancel w-100"
+            onClick={() => setIsContactOpen(false)}
+          >
+            Abbrechen
+          </button>
+        </div>
+
+        <span className="file-max-size">
+          Mit Absenden akzeptieren Sie unsere AGB.
+        </span>
+      </form>
+    );
   };
   return (
     <>
@@ -48,7 +328,10 @@ export default function About() {
                 Our mission, vision, and values guide our daily actions.
               </span>
 
-              <button className="btn-about-cable" onClick={handleContactClick}>Jetzt Anfragen</button>
+              <button className="btn-about-cable"  onClick={() => {
+                setIsFormSubmitted(false);
+                setIsContactOpen(true);
+              }}>Jetzt Anfragen</button>
             </div>
           </section>
 
@@ -347,7 +630,10 @@ export default function About() {
                 Our mission, vision, and values guide our daily actions.
               </span>
 
-              <button className="btn-about-cable mt-4 w-100" onClick={handleContactClick}>
+              <button className="btn-about-cable mt-4 w-100"  onClick={() => {
+                setIsFormSubmitted(false);
+                setIsContactOpen(true);
+              }}>
                 Jetzt Anfragen
               </button>
             </div>
@@ -584,6 +870,33 @@ export default function About() {
             </div>
           </section>
           <Footer />
+        </div>
+      )}
+        {isContactOpen && (
+        <div className="modal-overlay">
+          <div className="modal-container">
+            <img
+              src={cross}
+              alt="close"
+              className="modal-close cursor-pointer"
+              onClick={() => setIsContactOpen(false)}
+            />
+            {!isFormSubmitted && (
+              <>
+                <span className="modal-title-form">
+                  Senden Sie uns bitte Ihre Anfrage
+                </span>
+              </>
+            )}
+
+            {isFormSubmitted ? (
+              <SuccessView />
+            ) : isMobile ? (
+              <ContactFormMobile onSuccess={() => setIsFormSubmitted(true)} />
+            ) : (
+              <ContactFormWeb onSuccess={() => setIsFormSubmitted(true)} />
+            )}
+          </div>
         </div>
       )}
     </>
