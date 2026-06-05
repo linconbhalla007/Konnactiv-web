5fb3a3ded01ed3731da3fdf97c94ee37bbf87e79
godude <godude20@gmail.com>
2026-06-06 00:00:39 +0530

    terms page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Home from './Home/Home';
 import About from './About/About';
+import Terms from './Terms/Terms';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/agb" element={<Terms />} />
     </Routes>
   );
 }
